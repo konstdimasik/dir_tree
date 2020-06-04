c81734fed81807769455f9c88fb1c17739057154
konstdimasik <konstdimasik@github.com>
2020-06-05 00:58:49 +0300

    Добавил исключение .git, написал пояснительные комментарии, навел марафет

--- main.go
+++ main.go
@@ -8,6 +8,78 @@ import (
 	"log"
 )
 
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, filePath string, printFiles bool) error  {
+	var dirGraph = ""
+	err := levelDir(out, filePath, printFiles, dirGraph)
+
+	return err
+}
+
+//Рекурсивная ф-ия обрабатывающая отдельный уровень дерева файлов
+func levelDir(out io.Writer, filePath string, printFiles bool, dirGraph string) error {
+
+	var tempGraph string
+	var tempSize string
+	var printGraph string
+
+	files, err := ioutil.ReadDir(filePath)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+		if !file.IsDir() && !printFiles {
+			continue
+		}
+
+		if file.Name() == ".DS_Store" || file.Name() == ".git" {
+			continue
+		}
+
+		if isItLast(filePath, file, printFiles) {
+			printGraph = dirGraph + "└───" + file.Name()
+			tempGraph = "	"
+		} else {
+			printGraph = dirGraph + "├───"+file.Name()
+			tempGraph = "│	"
+		}
+
+		if !file.IsDir(){
+			if file.Size() == 0 {
+				tempSize = " (empty)"
+			} else {
+				tempSize = fmt.Sprintf(" (%vb)",file.Size())
+			}
+			printGraph = printGraph + tempSize
+		}
+
+	 	_, err := fmt.Fprintln(out, printGraph)
+		if err != nil {
+			 fmt.Fprintf(os.Stderr, "Fprintln: %v\n", err)
+		}
+
+		if file.IsDir() {
+			levelDir(out, filePath + "/" + file.Name(), printFiles, dirGraph + tempGraph)
+		}
+    }
+
+	return err
+}
+
+//Ф-ия проверяет является файл или папка последней на конкретном уровне дерева
 func isItLast (filePath string, f os.FileInfo, printFiles bool) bool {
 	files, err := ioutil.ReadDir(filePath)
     if err != nil {
@@ -34,76 +106,4 @@ func isItLast (filePath string, f os.FileInfo, printFiles bool) bool {
 		}
 	}
 	return false
-}
-
-func levelDir(out io.Writer, filePath string, printFiles bool, dirGraph string) error {
-
-	var tempGraph string
-	var tempSize string
-	var printGraph string
-
-	files, err := ioutil.ReadDir(filePath)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    for _, file := range files {
-		if !file.IsDir() && !printFiles {
-			continue
-		}
-
-		if file.Name() == ".DS_Store" {
-			continue
-		}
-
-		if isItLast(filePath, file, printFiles) {
-			printGraph = dirGraph + "└───" + file.Name()
-			tempGraph = "	"
-		} else {
-			printGraph = dirGraph + "├───"+file.Name()
-			tempGraph = "│	"
-		}
-
-		if !file.IsDir(){
-			if file.Size() == 0 {
-				tempSize = " (empty)"
-			} else {
-				tempSize = fmt.Sprintf(" (%vb)",file.Size())
-			}
-			printGraph = printGraph + tempSize
-		}
-
-		// fmt.Println(printGraph)
-
-	 	_, err := fmt.Fprintln(out, printGraph)
-		if err != nil {
-			 fmt.Fprintf(os.Stderr, "Fprintln: %v\n", err)
-		}
-
-		if file.IsDir() {
-			levelDir(out, filePath + "/" + file.Name(), printFiles, dirGraph + tempGraph)
-		}
-    }
-
-	return err
-}
-
-func dirTree(out io.Writer, filePath string, printFiles bool) error  {
-	var dirGraph = ""
-	err := levelDir(out, filePath, printFiles, dirGraph)
-
-	return err
-}
-
-func main() {
-	out := os.Stdout
-	if !(len(os.Args) == 2 || len(os.Args) == 3) {
-		panic("usage go run main.go . [-f]")
-	}
-	path := os.Args[1]
-	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
-	err := dirTree(out, path, printFiles)
-	if err != nil {
-		panic(err.Error())
-	}
 }
